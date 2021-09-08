c045aaec5a488d10970473cfc30dd5ec1411cd69
ankitoscar <ankitoscar911@gmail.com>
2021-09-08 12:48:22 +0530

    Added views for homepage and uploader

--- app/uploader/views.py
+++ app/uploader/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'uploader/homepage.html')
+
+def upload(request):
+    return render(request, 'uploader/main.html')
