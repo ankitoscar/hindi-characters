65789a36b440b50067d945115df8f2e6470a41d9
ankitoscar <ankitoscar911@gmail.com>
2021-09-09 23:25:38 +0530

    Added photo upload view

--- app/uploader/views.py
+++ app/uploader/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ImageForm
+
 
 # Create your views here.
 
@@ -6,4 +8,14 @@ def index(request):
     return render(request, 'uploader/homepage.html')
 
 def upload(request):
-    return render(request, 'uploader/main.html')
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+
+            img_obj = form.instance
+            return render(request, 'uploader/main.html', {'form': form, 'img_obj': img_obj})
+
+    else:
+        form = ImageForm()
+    return render(request, 'uploader/main.html', {'form': form})
