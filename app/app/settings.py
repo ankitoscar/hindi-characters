e5dea11e24ab86983bf79b57f02be81c871d81c1
ankitoscar <ankitoscar911@gmail.com>
2021-09-09 23:24:19 +0530

    Added app name to installed app and fixed urls

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'uploader.apps.UploaderConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
     '/var/www/static/',
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR.joinpath('media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
